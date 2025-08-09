8634778bff60419c6f7e99e6646dceb6b7c5759d
Shibbir Ahmed <shibbirasif@gmail.com>
2025-08-09 15:17:11 +0600

    refactor: enhance Topbar component with user session handling and UI improvements

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,8 +1,7 @@
-import NavLinks from "./navigations/NavLinks";
 import { auth } from "@/lib/auth";
 import SignOut from "./auth/SignOut";
 import SignIn from "./auth/SignIn";
-import { Avatar, DarkThemeToggle, Dropdown, DropdownDivider, DropdownHeader, DropdownItem, Navbar, NavbarBrand, NavbarToggle } from "flowbite-react";
+import { Avatar, Button, DarkThemeToggle, Dropdown, DropdownDivider, DropdownHeader, DropdownItem, Navbar, NavbarBrand } from "flowbite-react";
 import Link from "next/link";
 import Image from "next/image";
 import SidebarToggle from "./ui/SidebarToggle";
@@ -33,33 +32,40 @@ export default async function Topbar({ sidebarId }: TopbarProps) {
                 </NavbarBrand>
             </div>
             <div className="flex md:order-2">
-                <DarkThemeToggle className="mx-2" />
+                {session?.user &&
+                    <Button href={APP_ROUTES.USER.ARTICLE.NEW(session.user.id)} color="pink">
+                        New Article
+                    </Button>
+                }
+                <DarkThemeToggle className="mx-2 cursor-pointer" />
                 {session?.user ?
                     <Dropdown
                         arrowIcon={false}
                         inline
+                        className="w-[200px]"
                         label={
-                            session?.user?.avatar && session.user.avatar !== '' ? (
-                                <Avatar alt="User settings" img={session.user.avatar} rounded />
-                            ) : (
-                                <Avatar placeholderInitials={session?.user ? session.user.name[0].toUpperCase() : "U"} rounded />
-                            )
+                            session?.user?.avatar && session.user.avatar !== '' ?
+                                <Avatar alt="User settings" img={session.user.avatar} rounded className="cursor-pointer" />
+                                :
+                                <Avatar placeholderInitials={session?.user ? session.user.name[0].toUpperCase() : "U"} rounded className="cursor-pointer" />
+
                         }
                     >
                         <DropdownHeader>
-                            <span className="block text-sm">{session.user.name}</span>
+                            <span className="block text-xl">{session.user.name}</span>
                             <span className="block truncate text-sm font-medium">{session.user.email}</span>
                         </DropdownHeader>
+                        <DropdownDivider />
                         <DropdownItem><Link href={APP_ROUTES.USER.FEED(session.user.id)}>Dashboard</Link></DropdownItem>
                         <DropdownItem><Link href={APP_ROUTES.USER.PROFILE_EDIT(session.user.id)}>Settings</Link></DropdownItem>
                         <DropdownDivider />
                         <SignOut>Sign out</SignOut>
                     </Dropdown>
                     : <SignIn>Sign In</SignIn>}
-                <NavbarToggle className="mx-2" />
+                {/* <NavbarToggle className="mx-2" /> //Removing for now, we will add it back later\\ */}
             </div>
 
-            <NavLinks />
+            {/* <NavLinks /> //Removing for now, we will add it back later\\*/}
         </Navbar>
     )
 }
